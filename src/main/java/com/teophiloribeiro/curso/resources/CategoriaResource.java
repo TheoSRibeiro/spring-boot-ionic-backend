78e344ed4a48f9f46e54ac028be20a44f21b53a1
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-05-01 17:02:34 -0300

    Listando todas as Categorias com DTO

--- src/main/java/com/teophiloribeiro/curso/resources/CategoriaResource.java
+++ src/main/java/com/teophiloribeiro/curso/resources/CategoriaResource.java
@@ -1,6 +1,8 @@
 package com.teophiloribeiro.curso.resources;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.teophiloribeiro.curso.domain.Categoria;
+import com.teophiloribeiro.curso.dto.CategoriaDTO;
 import com.teophiloribeiro.curso.services.CategoriaService;
 
 //CONTROLADOR REST
@@ -51,6 +54,26 @@ public class CategoriaResource {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
 	}
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public ResponseEntity<List<CategoriaDTO>> findAll() {
+		List<Categoria> list = service.findAll();
+		//converter uma lista com varios atributos indesejados em outra somente com os atributos desejados (id e nome)
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
