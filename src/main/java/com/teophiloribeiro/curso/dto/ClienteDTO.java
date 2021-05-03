5b39d5d17f7fc5576ba551650434fe1cc599b60f
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-05-02 21:43:18 -0300

    Validação customizada: email não repetido

--- src/main/java/com/teophiloribeiro/curso/dto/ClienteDTO.java
+++ src/main/java/com/teophiloribeiro/curso/dto/ClienteDTO.java
@@ -7,7 +7,9 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 
 import com.teophiloribeiro.curso.domain.Cliente;
+import com.teophiloribeiro.curso.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
