aca7abc238722f561b00c8529bf6e4015d3d5986
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-05-10 14:31:01 -0300

    Configuração inicial do Spring Security

--- src/main/java/com/teophiloribeiro/curso/services/MockEmailService.java
+++ src/main/java/com/teophiloribeiro/curso/services/MockEmailService.java
@@ -10,6 +10,7 @@ public class MockEmailService extends AbstractEmailService{
     
     private static final Logger LOG = LoggerFactory.getLogger(MockEmailService.class);
 
+    @Override
     public void sendEmail(SimpleMailMessage msg){
         LOG.info("Simulando envio de email...");
         LOG.info(msg.toString());
