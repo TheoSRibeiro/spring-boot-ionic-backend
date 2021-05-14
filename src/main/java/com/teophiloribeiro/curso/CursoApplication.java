1a8779fa19675dca3aaba56143d3a5ddd84c27ae
Teophilo Ribeiro <theosribeiro@gmail.com>
2021-05-14 20:42:49 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/teophiloribeiro/curso/CursoApplication.java
+++ src/main/java/com/teophiloribeiro/curso/CursoApplication.java
@@ -1,5 +1,8 @@
 package com.teophiloribeiro.curso;
 
+import com.teophiloribeiro.curso.services.S3Service;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +12,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CursoApplication implements CommandLineRunner{
 	
+	@Autowired
+	private S3Service s3Service;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursoApplication.class, args);
 	}
@@ -16,7 +22,7 @@ public class CursoApplication implements CommandLineRunner{
 	//Metodo para instanciar o BD com as categorias e produtos
 	@Override
 	public void run(String... args) throws Exception {
-
+		s3Service.uploadFile("D:\\Imagens\\bios.png");
 	}
 	
 
